74824d3d7da5858b2a6b45cebc91666a530f19a6
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-11-14 16:02:28 +0530

    Brute Force Approach

---
+++ src/com/recursion_program/First_UpperCase_Letter_String.java
@@ -0,0 +1,23 @@
+package com.recursion_program;
+
+public class First_UpperCase_Letter_String {
+	public static char checkUpper(String str)
+	{
+		String clone = str;
+		clone = clone.toUpperCase();
+		
+		for(int i = 0; i< str.length(); i++)
+		{
+			if(str.charAt(i) == clone.charAt(i))
+			{
+				//System.out.println(str.charAt(i));
+				return str.charAt(i);
+			}
+		}
+		return 0;
+	}
+	public static void main(String[] args) {
+		String str= "geekS";
+		System.out.println(checkUpper(str));
+	}
+}
