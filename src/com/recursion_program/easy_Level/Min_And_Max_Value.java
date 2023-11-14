c245cd33f16bbb2c58e0e6832c43137146849350
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-11-14 15:22:03 +0530

    recursion approach

--- src/com/recursion_program/easy_Level/Min_And_Max_Value.java
+++ src/com/recursion_program/easy_Level/Min_And_Max_Value.java
@@ -3,15 +3,7 @@ package com.recursion_program.easy_Level;
 import java.util.TreeSet;
 
 public class Min_And_Max_Value {
-	public static void main(String[] args) {
-		long array[] = {1, 4, 3, -5, -4, 8, 6};
-		int[] minMax = getMinMax(array);
-		for (int i : minMax) {
-			System.out.print(i+" ");
-		}
-	}
 	
-
 	static int[] getMinMax(long a[])  
     {
         
@@ -25,6 +17,40 @@ public class Min_And_Max_Value {
 		
         return new int[] {min,max};
     }
+	
+	
+	//new approch
+	public static int findMinRec(int A[], int n) 
+    { 
+      // if size = 0 means whole array 
+      // has been traversed 
+      if(n == 1) 
+        return A[0]; 
+          
+        return Math.min(A[n-1], findMinRec(A, n-1)); 
+    } 
+      
+	public static int findMaxRec(int A[], int n) 
+    { 
+      // if size = 0 means whole array 
+      // has been traversed 
+      if(n == 1) 
+        return A[0]; 
+          
+        return Math.max(A[n-1], findMaxRec(A, n-1)); 
+    } 
+    // Driver code 
+    public static void main(String args[]) 
+    { 
+        int A[] = {1, 4, 3, -5, -4, 8, 6}; 
+        int n = A.length; 
+          
+        // Function calling 
+        		int min = (findMinRec(A, n)); 
+        		int max = findMaxRec(A,n);
+        		
+        		System.out.println("the min: "+ min+" and max :"+ max);
+    } 
 }
 /*
 Input: arr = {1, 4, 3, -5, -4, 8, 6};
