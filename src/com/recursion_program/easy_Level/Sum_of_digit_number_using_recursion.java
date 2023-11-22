070818ce0c1c023c4aa4d99cd5e4fbeb1bd798a5
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-11-22 16:03:13 +0530

    linear appoach and recursive approach

---
+++ src/com/recursion_program/easy_Level/Sum_of_digit_number_using_recursion.java
@@ -0,0 +1,28 @@
+package com.recursion_program.easy_Level;
+
+public class Sum_of_digit_number_using_recursion {
+	public static int sum_of_number(int num)
+	{
+		/*int sum = 0;
+		while(num > 0)
+		{
+			int rem = num %10;
+			sum = rem+sum;
+			num = num/10;
+		}
+		System.out.println(sum);*/
+		
+		if(num == 0)
+		{
+			return 0;
+		}
+	
+		return (num%10 +(sum_of_number(num/10)));
+	}
+
+	public static void main(String[] args) {
+		int number = 12345;
+		System.out.println(sum_of_number(number));
+	}
+
+}
