4ce77c16928a0dabb80a472343bc56c6e436f63d
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-11-24 18:38:39 +0530

    formate the code

--- src/com/recursion_program/easy_Level/Product_of_2_Numbers_using_Recursion.java
+++ src/com/recursion_program/easy_Level/Product_of_2_Numbers_using_Recursion.java
@@ -1,13 +1,11 @@
 package com.recursion_program.easy_Level;
 
 public class Product_of_2_Numbers_using_Recursion {
-
 	public static void main(String[] args) {
 		int num1 = 100;
 		int num2 = 5;
 
 		int result = multiply(num1, num2);
-
 		System.out.println("Product of " + num1 + " and " + num2 + " is: " + result);
 	}
 
@@ -21,7 +19,6 @@ public class Product_of_2_Numbers_using_Recursion {
 		// Recursive case: a * b = a + a * (b-1)
 		return a + multiply(a, b - 1);
 	}
-
 }
 
 /*
@@ -32,17 +29,17 @@ public class Product_of_2_Numbers_using_Recursion {
  */
 /* 
 
-											RRT
-											multiply(3, 4)
-											|-- 3 + multiply(3, 3)
-											|   |-- 3 + multiply(3, 2)
-											|   |   |-- 3 + multiply(3, 1)
-											|   |   |   |-- 3 + multiply(3, 0)  // Base case: b == 0
-											|   |   |   |   `-- Result: 0
-											|   |   |   `-- Result: 3
-											|   |   `-- Result: 6
-											|   `-- Result: 9
-											`-- Result: 12				
+			RRT
+			multiply(3, 4)
+			|-- 3 + multiply(3, 3)
+			|   |-- 3 + multiply(3, 2)
+			|   |   |-- 3 + multiply(3, 1)
+			|   |   |   |-- 3 + multiply(3, 0)  // Base case: b == 0
+			|   |   |   |   `-- Result: 0
+			|   |   |   `-- Result: 3
+			|   |   `-- Result: 6
+			|   `-- Result: 9
+			`-- Result: 12				
 
 
 */
