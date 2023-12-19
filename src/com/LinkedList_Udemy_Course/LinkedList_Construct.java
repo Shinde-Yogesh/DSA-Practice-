92509909b2a141a0ec6b8ecbb7e870abf38f25ce
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-12-19 21:06:56 +0530

    creating the Linked List package with program

---
+++ src/com/LinkedList_Udemy_Course/LinkedList_Construct.java
@@ -0,0 +1,61 @@
+package com.LinkedList_Udemy_Course;
+
+public class LinkedList_Construct {
+	
+	private Node head;
+	private Node tail;
+	private int length;
+	
+	class Node{
+		int value;
+		Node next;
+		
+		Node(int value)
+		{
+			this.value = value;
+		}
+	}
+	
+	public LinkedList_Construct(int value)
+	{
+		Node newNode = new Node(value);
+		head = newNode;
+		tail = newNode;
+		length = 1;
+	}
+	
+	public void printList()
+	{
+		Node temp = head;
+		while(temp != null)
+		{
+			System.out.println(temp.value);
+			temp = temp.next;
+		}
+	}
+	
+	public void getHead()
+	{
+		System.out.println("Head : "+head.value);
+	}
+	
+	public void gettail()
+	{
+		System.out.println("Tail : "+ tail.value);
+	}
+	public void getLength()
+	{
+		System.out.println("Length : "+length);
+	}
+
+	// main method
+	public static void main(String[] args) {
+		LinkedList_Construct myLinkedList = new LinkedList_Construct(4);
+		myLinkedList.getHead();
+		myLinkedList.gettail();
+		myLinkedList.getLength();
+		
+		
+		myLinkedList.printList();
+	}
+}
