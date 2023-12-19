c94944cfdc5f7bd5d5a77c1e170b783402df20f6
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-12-19 21:38:11 +0530

    remove last element

--- src/com/LinkedList_Udemy_Course/Append_Node_Last.java
+++ src/com/LinkedList_Udemy_Course/Append_Node_Last.java
@@ -43,13 +43,51 @@ public class Append_Node_Last {
 		}
 		length++;
 	}
+	
+	public Node removeLast()
+	{
+		if(length == 0)
+		{
+			return null;
+		}
+		
+		Node temp = head;
+		Node pre = head;
+		
+		while(temp.next != null)
+		{
+			pre = temp;
+			temp = temp.next;
+		}
+		tail = pre;
+		tail.next = null;
+		length--;
+		
+		// if single node test case
+		if(length == 0)
+		{
+			head = null;
+			tail = null;
+		}
+		return temp;
+	}
 
 	public static void main(String[] args) {
 		Append_Node_Last myLinkedList = new Append_Node_Last(4);
-		myLinkedList.printList();
+//		myLinkedList.printList();
 
 		myLinkedList.append(5);
-		myLinkedList.printList();
+//		myLinkedList.printList();
+		
+		
+		// if 2 Item - return 2 node
+		System.out.println(myLinkedList.removeLast().value);
+		
+		// if 1Item - return 1 node
+		System.out.println(myLinkedList.removeLast().value);
+		
+		// if 0 Item - return null
+		System.out.println(myLinkedList.removeLast());
 	}
 
 }
