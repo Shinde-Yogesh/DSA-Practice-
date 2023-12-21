e23e269bf6b0e7c73525eef0e82aab2ba1c2b446
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-12-21 19:24:02 +0530

    insert the node and remove the node method added

--- src/com/LinkedList_Udemy_Course/Get_Node_Index.java
+++ src/com/LinkedList_Udemy_Course/Get_Node_Index.java
@@ -1,5 +1,7 @@
 package com.LinkedList_Udemy_Course;
 
+import com.LinkedList_Udemy_Course.Append_Node_Last.Node;
+
 public class Get_Node_Index {
 
 	private Node head;
@@ -14,6 +16,44 @@ public class Get_Node_Index {
 			this.value = value;
 		}
 	}
+	// remove first element
+		public Node removeFirst() {
+			if (length == 0)
+				return null;
+			Node temp = head;
+			head = head.next;
+			temp.next = null;
+			length--;
+			if (length == 0) {
+				tail = null;
+			}
+			return temp;
+		}
+
+		public Node removeLast() {
+			if (length == 0) {
+				return null;
+			}
+
+			Node temp = head;
+			Node pre = head;
+
+			while (temp.next != null) {
+				pre = temp;
+				temp = temp.next;
+			}
+			tail = pre;
+			tail.next = null;
+			length--;
+
+			// if single node test case
+			if (length == 0) {
+				head = null;
+				tail = null;
+			}
+			return temp;
+		}
+
 
 	public Get_Node_Index(int value) {
 		Node newNode = new Node(value);
@@ -64,6 +104,48 @@ public class Get_Node_Index {
 		}
 		return false;
 	}
+	
+	// Insert the node 
+	public boolean insert(int index, int value)
+	{
+		if(index<0 || index>length) return false;
+		if(index == 0)
+		{
+			append(value);
+			return true;
+		}
+		if(index == length)
+		{
+			append(value);
+			return true;
+		}
+		
+		Node newNode = new Node(value);
+		Node temp = get(index -1);
+		newNode.next = temp.next;
+		temp.next = newNode;
+		length++;
+	return true;
+	}
+	
+	//Remove the Node
+	public Node remove(int index)
+	{
+		if(index < 0 || index >= length) return null;
+		if(index == 0) return removeFirst();
+		if(index == length-1) return removeLast();
+
+	
+	Node prev = get(index -1);
+	Node temp = prev.next;
+	
+	prev.next = temp.next;
+	temp.next = null;
+	length--;
+	return temp;
+	
+	
+	}
 
 	public void getLength() {
 		System.out.println("Length : " + length);
@@ -78,9 +160,12 @@ public class Get_Node_Index {
 		myLinkedList.append(8);
 		myLinkedList.getLength();
 		
-		myLinkedList.printList();
-		myLinkedList.set(4,9);
+//		myLinkedList.printList();
+//		myLinkedList.set(4,9);
 		
+		myLinkedList.printList();
+//		myLinkedList.insert(1, 3);
+		System.out.println("The remove node is : "+myLinkedList.remove(2).value);
 		myLinkedList.printList();
 //		System.out.println("The index element : "+myLinkedList.get(2).value);
 	}
