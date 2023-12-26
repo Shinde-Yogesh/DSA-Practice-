d04958bd0c040c6ceada1efcdc658bb59ae515b4
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-12-26 20:45:18 +0530

    Doubly Linked List Constuctor initialize and print value

---
+++ src/com/DoublyLinkedList_Udemy_Course/Main.java
@@ -0,0 +1,12 @@
+package com.DoublyLinkedList_Udemy_Course;
+
+public class Main {
+	public static void main(String[] args) {
+		DoublyLinkedList myDLL = new DoublyLinkedList(7);
+		myDLL.getHead();
+		myDLL.getTail();
+		;
+		myDLL.printList();
+		;
+	}
+}
