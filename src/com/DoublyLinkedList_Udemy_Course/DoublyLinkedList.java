e8d95d68379250deb689dd671af65b34bdf234bb
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-12-28 22:19:40 +0530

    searching elemenet in efficency way

--- src/com/DoublyLinkedList_Udemy_Course/DoublyLinkedList.java
+++ src/com/DoublyLinkedList_Udemy_Course/DoublyLinkedList.java
@@ -39,56 +39,42 @@ public class DoublyLinkedList {
 	public void getTail() {
 		System.out.println("Tail : " + tail.value);
 	}
+
 	public void getLength() {
 		System.out.println("Length : " + length);
 	}
-	
+
 	// Append the Node
-	public void append(int value)
-	{
+	public void append(int value) {
 		Node newNode = new Node(value);
-		
-		if(length == 0)
-		{
+
+		if (length == 0) {
 			head = newNode;
 			tail = newNode;
-		}
-		else
-		{
+		} else {
 			tail.next = newNode;
 			newNode.prev = tail;
 			tail = newNode;
 		}
 		length++;
 	}
-	
-	//removeLast Node
-	public Node removeLast()
-	{/*
-		if(length == 0) return null;
+
+	// removeLast Node
+	public Node removeLast() {/*
+								 * if(length == 0) return null; Node temp = tail; tail = temp.prev; tail.next =
+								 * null; temp.prev = null; length--; // One test case for after remove single
+								 * node in linked list if(length == 0) { head = null; tail = null; } return
+								 * temp;
+								 */
+
+		// optimize code
+		if (length == 0)
+			return null;
 		Node temp = tail;
-		tail = temp.prev;
-		tail.next = null;
-		temp.prev = null;
-		length--;
-		// One test case for after remove single node in linked list
-		if(length == 0)
-		{
+		if (length == 1) {
 			head = null;
 			tail = null;
-		}
-		return temp;*/
-		
-		//optimize code
-		if(length == 0) return null;
-		Node temp = tail;
-		if(length == 1)
-		{
-			head = null;
-			tail = null;
-		}
-		else
-		{
+		} else {
 			tail = temp.prev;
 			tail.next = null;
 			temp.prev = null;
@@ -96,38 +82,53 @@ public class DoublyLinkedList {
 		length--;
 		return temp;
 	}
-	
-	public void prepend(int value)
-	{
+
+	public void prepend(int value) {
 		Node newNode = new Node(value);
-		if(length == 0)
-		{
+		if (length == 0) {
 			head = newNode;
 			tail = newNode;
-		}else
-		{
+		} else {
 			newNode.next = head;
 			head.prev = newNode;
 			head = newNode;
 		}
 		length++;
 	}
-	
-	public Node removeFirst()
-	{
-		if(length == 0) return null;
+
+	public Node removeFirst() {
+		if (length == 0)
+			return null;
 		Node temp = head;
-		if(length == 1)
-		{
+		if (length == 1) {
 			head = null;
 			tail = null;
-		}else
-		{
+		} else {
 			head = head.next;
 			head.prev = null;
 			temp.next = null;
 		}
 		length--;
+		return temp;
+	}
+	// get the value using doubly linked list -> efficiecnt way
+
+	public Node get(int index) {
+		if (index < 0 || index > length) {
+			return null;
+		}
+
+		Node temp = head;
+		if (index < length / 2) {
+			for (int i = 0; i < index; i++) {
+				temp = temp.next;
+			}
+		} else {
+			temp = tail;
+			for (int i = length - 1; i > index; i--) {
+				temp = temp.prev;
+			}
+		}
 		return temp;
 	}
 }
--- src/com/DoublyLinkedList_Udemy_Course/Main.java
+++ src/com/DoublyLinkedList_Udemy_Course/Main.java
@@ -3,18 +3,19 @@ package com.DoublyLinkedList_Udemy_Course;
 public class Main {
 	public static void main(String[] args) {
 		DoublyLinkedList myDLL = new DoublyLinkedList(7);
-		myDLL.getHead();
+//		myDLL.getHead();
 		
 		myDLL.append(8);
 		myDLL.append(9);
 		myDLL.append(10);
-		myDLL.getTail();
+//		myDLL.getTail();
 //		myDLL.printList();
 //		myDLL.removeLast();
-		myDLL.printList();
+//		myDLL.printList();
 		myDLL.prepend(6);
-		myDLL.printList();
-		myDLL.removeFirst();
+		System.out.println(myDLL.get(2).value);
+		System.out.println(myDLL.get(4).value);
+//		myDLL.removeFirst();
 		myDLL.printList();
 	}
 }
