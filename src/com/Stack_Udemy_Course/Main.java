66e7fa85df9143b97f1c3837f2a5b1982c1cb642
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-12-29 19:20:08 +0530

    pop the node using temp node

--- src/com/Stack_Udemy_Course/Main.java
+++ src/com/Stack_Udemy_Course/Main.java
@@ -3,11 +3,16 @@ package com.Stack_Udemy_Course;
 public class Main {
 	public static void main(String[] args) {
 		Stack stack = new Stack(4);
-		
+		stack.push(5);
+		stack.push(6);
+		stack.push(7);
+		stack.push(8);
 		stack.getTop();
 		stack.getHeight();
 		stack.printStack();
 		
+		stack.pop();
+		stack.printStack();
 	}
 
 }
--- src/com/Stack_Udemy_Course/Stack.java
+++ src/com/Stack_Udemy_Course/Stack.java
@@ -1,45 +1,69 @@
 package com.Stack_Udemy_Course;
 
 public class Stack {
-	
+
 	private Node top;
 	private int height;
-	
-	class Node
-	{
+
+	class Node {
 		int value;
 		Node next;
-		
-		Node(int value)
-		{
+
+		Node(int value) {
 			this.value = value;
 		}
 	}
-	
-	public Stack(int value)
-	{
+
+	public Stack(int value) {
 		Node newNode = new Node(value);
 		top = newNode;
 		height = 1;
 	}
-	
-	public void printStack()
-	{
+
+	public void printStack() {
 		Node temp = top;
-		while(temp != null)
-		{
-			System.out.println(temp.value);
+		while (temp != null) {
+			System.out.print(temp.value + " -> ");
 			temp = temp.next;
 		}
+		System.out.println("START");
 	}
-	
-	public void getTop()
-	{
-		System.out.println("Top : "+top.value);
+
+	public void getTop() {
+		System.out.println("Top : " + top.value);
 	}
-	public void getHeight()
-	{
-		System.out.println("height : "+height);
+
+	public void getHeight() {
+		System.out.println("height : " + height);
+	}
+
+	// push the new node
+
+	public void push(int value) {
+		Node newNode = new Node(value);
+
+		if (height == 0) {
+			top = newNode;
+		} else {
+			newNode.next = top;
+			top = newNode;
+		}
+		height++;
+	}
+
+	// pop the node
+
+	public Node pop() {
+
+		if (height == 0) {
+			return null;
+		}
+		Node temp = top;
+		top = top.next;
+		temp.next = null;
+		height--;
+		return temp;
+
 	}
 
 }
