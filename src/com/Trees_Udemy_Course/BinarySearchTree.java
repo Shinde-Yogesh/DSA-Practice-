d4c6d078e5af1d94f1daef0e6b71ac14217407a6
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-12-30 17:00:27 +0530

    insert the node using temp and left and right techniqu

--- src/com/Trees_Udemy_Course/BinarySearchTree.java
+++ src/com/Trees_Udemy_Course/BinarySearchTree.java
@@ -14,6 +14,47 @@ public class BinarySearchTree {
 		}
 	}
 	
+	// for the insert the node
+	public boolean insert(int value)
+	{
+		Node newNode = new Node(value);
+		
+		// base case
+		if(root == null)
+		{
+			root =newNode;
+			return true;
+		}
+		
+		//if not then
+		Node temp = root;
+		while(true)
+		{
+			// IMP case if inserting value same as node in Tree return false
+			if(newNode.value == temp.value) return false;
+			
+			//for left condition
+			if(newNode.value < temp.value)
+			{
+				// if the left node is empty then 
+				if(temp.left == null)
+				{
+					temp.left = newNode;
+					return true;
+				}
+				//else the temp move the next node and process goes on until result not fount
+				temp = temp.left;
+			}else
+			{
+				if(temp.right == null)
+				{
+					temp.right = newNode;
+					return true;
+				}
+				temp = temp.right;
+			}
+		}
+	}
 	
 
 }
--- src/com/Trees_Udemy_Course/Main.java
+++ src/com/Trees_Udemy_Course/Main.java
@@ -4,7 +4,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		BinarySearchTree tree = new BinarySearchTree();
-		System.out.println("root : "+ tree.root);
+//		System.out.println("root : "+ tree.root);
+		tree.insert(47);
+		tree.insert(21);
+		tree.insert(76);
+		tree.insert(18);
+		tree.insert(52);
+		tree.insert(82);
+		
+		tree.insert(27);
+		
+		System.out.println(tree.root.left.right.value);
 	}
 	
 }
