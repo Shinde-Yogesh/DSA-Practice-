d3bd2287ae3de04c99a48e1ff0b24f7665d28bd3
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-12-30 13:10:12 +0530

    Trees New Module In DSA with constructor

---
+++ src/com/Trees_Udemy_Course/BinarySearchTree.java
@@ -0,0 +1,19 @@
+package com.Trees_Udemy_Course;
+
+public class BinarySearchTree {
+	 Node root;
+	
+	class Node{
+		int value;
+		Node left;
+		Node right;
+		
+		Node(int value)
+		{
+			this.value = value;
+		}
+	}
+	
+	
+
+}
---
+++ src/com/Trees_Udemy_Course/Main.java
@@ -0,0 +1,10 @@
+package com.Trees_Udemy_Course;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BinarySearchTree tree = new BinarySearchTree();
+		System.out.println("root : "+ tree.root);
+	}
+	
+}
