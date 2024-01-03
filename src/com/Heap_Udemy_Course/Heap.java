6630b2aa693606649d3a3a5c7421740837dea990
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-01-03 13:00:34 +0530

    creating the Heap module and some basic method, the arraylist is used to store and manipulated the value

---
+++ src/com/Heap_Udemy_Course/Heap.java
@@ -0,0 +1,36 @@
+package com.Heap_Udemy_Course;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Heap {
+	private List<Integer> heap;
+
+	public Heap() {
+		this.heap = new ArrayList<>();
+	}
+
+	public List<Integer> getHeap() {
+		return new ArrayList<>(heap);
+	}
+
+	private int leftChild(int index) {
+		return 2 * index + 1;
+	}
+
+	private int rightChild(int index) {
+		return 2 * index + 2;
+	}
+
+	private int parent(int index) {
+		return (index - 1) / 2;
+	}
+
+	// for swape the value
+	private void swap(int index1, int index2) {
+		int temp = heap.get(index1);
+		heap.set(index1, heap.get(index2));
+		heap.set(index2, temp);
+	}
+
+}
