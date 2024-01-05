841fd2d6cef5ba06376dd7c7a8a8ca178845b64d
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-01-05 11:35:15 +0530

    remove the top value using sinkDown method were swap the greter note at top

--- src/com/Heap_Udemy_Course/Heap.java
+++ src/com/Heap_Udemy_Course/Heap.java
@@ -9,7 +9,7 @@ public class Heap {
 	public Heap() {
 		this.heap = new ArrayList<>();
 	}
- 
+
 	public List<Integer> getHeap() {
 		return new ArrayList<>(heap);
 	}
@@ -32,17 +32,54 @@ public class Heap {
 		heap.set(index1, heap.get(index2));
 		heap.set(index2, temp);
 	}
-	
-	public void insert(int value)
-	{
+
+	public void insert(int value) {
 		heap.add(value);
-		int current = heap.size()-1;
-		
-		while(current > 0 && heap.get(current) > heap.get(parent(current)))
-		{
-			swap(current,parent(current));
+		int current = heap.size() - 1;
+
+		while (current > 0 && heap.get(current) > heap.get(parent(current))) {
+			swap(current, parent(current));
 			current = parent(current);
 		}
+	}
+
+	private void sinkDown(int index) {
+		int maxIndex = index;
+		while (true) {
+			int leftIndex = leftChild(index);
+			int rightIndex = rightChild(index);
+
+			if (leftIndex < heap.size() && heap.get(leftIndex) > heap.get(maxIndex)) {
+				maxIndex = leftIndex;
+			}
+			if (rightIndex < heap.size() && heap.get(rightIndex) > heap.get(maxIndex)) {
+				maxIndex = rightIndex;
+			}
+
+			if (maxIndex != index) {
+				swap(index, maxIndex);
+				index = maxIndex;
+			} else {
+				return;
+			}
+		}
+	}
+
+	// for remove the node
+	public Integer remove() {
+		if (heap.size() == 0) {
+			return null;
+		}
+		if (heap.size() == 1) {
+			return heap.remove(0);
+		}
+
+		int maxValue = heap.get(0);
+		// temp set the last node to top in heap
+		heap.set(0, heap.remove(heap.size() - 1));
+		// here is operation for for right node the top after the remove method
+		sinkDown(0);
+		return maxValue;
 	}
 
 }
--- src/com/Heap_Udemy_Course/Main.java
+++ src/com/Heap_Udemy_Course/Main.java
@@ -3,14 +3,22 @@ package com.Heap_Udemy_Course;
 public class Main {
 	public static void main(String[] args) {
 		Heap heap = new Heap();
-		heap.insert(99);
-		heap.insert(72);
-		heap.insert(61);
-		heap.insert(58);
+		heap.insert(95);
+		heap.insert(75);
+		heap.insert(80);
+		heap.insert(55);
+		heap.insert(60);
+		heap.insert(50);
+		heap.insert(65);
 
 		System.out.println(heap.getHeap());
 
-		heap.insert(100);
+		heap.remove();
 		System.out.println(heap.getHeap());
+		
+		heap.remove();
+		System.out.println(heap.getHeap());
+		
+//		System.out.println(heap.getHeap());
 	}
 }
