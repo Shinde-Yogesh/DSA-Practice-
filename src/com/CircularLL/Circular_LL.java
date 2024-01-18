f5962c96470ff9f611a2ced4817772a88a12d734
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-01-18 23:30:15 +0530

    creating the package with CLL

---
+++ src/com/CircularLL/Circular_LL.java
@@ -0,0 +1,17 @@
+package com.CircularLL;
+
+public class Circular_LL {
+	class Node{
+		int value;
+		Node next;
+		
+		public Node(int value)
+		{
+			this.value = value;
+			next = null;
+		}
+	}
+
+	
+	
+}
