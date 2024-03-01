6746a3cc37951021640af3115edcee40eb3455bd
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-03-01 21:29:54 +0530

    Using the Linked List Approach

--- src/com/queue_program/Queue_Imple_Using_Array.java
+++ src/com/queue_program/Queue_Imple_Using_Array.java
@@ -1,6 +1,6 @@
 package com.queue_program;
 
-public class Queue_Imple_Using_Array {
+/*public class Queue_Imple_Using_Array {
 	int array[];
 	int qfront;
 	int rear;
@@ -97,5 +97,119 @@ public class Queue_Imple_Using_Array {
 			System.out.print(i+ " ");
 		}
 		
+	}*/
+	
+	
+	/*
+    Time complexity: O(q)
+    Space complexity: O(q)
+
+    Where q is the number of queries.
+*/
+
+class Node {
+
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        next = null;
+    }
+};
+
+public class Queue_Imple_Using_Array {
+    Node head;
+    Node tail;
+    int size;
+
+    Queue_Imple_Using_Array() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    // Function to check if the queue is empty.
+    boolean isEmpty() {
+        return size == 0;
+    }
+
+    void enqueue(int data) {
+
+        // Increase the count of elements present in the List.
+        size++;
+
+        // Create a new node.
+        Node newNode = new Node(data);
+
+        // Check if the Queue is empty.
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+
+        // Push the element to the last of the queue.
+        tail.next = newNode;
+        tail = newNode;
+        return;
+    }
+
+    int dequeue() {
+
+        // Check if the queue is empty.
+        if (isEmpty()) {
+            return -1;
+        }
+
+        int ans = head.data;
+
+        Node tmp = head;
+        head = head.next;
+
+        // If the queue is empty make the tail pointer null.
+        if (head == null) {
+            tail = null;
+        }
+
+        tmp = null;
+
+        size--;
+
+        return ans;
+    }
+
+    int front() {
+
+        // Check if the queue is empty.
+        if (isEmpty()) {
+            return -1;
+        }
+
+        // Return the element at the front.
+        return head.data;
+    }
+//    print the queue using linked list
+    public  void print()
+    {
+    	Node temp = head;
+    	while(temp != tail)
+    	{
+    		System.out.print(temp.data + " "+" -> ");
+    		temp = temp.next;
+    	}
+    	System.out.print(tail.data +" -> ");
+    	System.out.println("REAR");
+    }
+    public static void main(String[] args) {
+    	Queue_Imple_Using_Array obj  = new Queue_Imple_Using_Array();
+    	obj.enqueue(17);
+    	obj.enqueue(19);
+    	obj.enqueue(23);
+    	System.out.println(obj.isEmpty());
+    	obj.print();
 	}
+
 }
+
+//}
