6210bd1d0470e150af24e09f126b444dae9a6da7
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-03-18 18:49:24 +0530

    improve the time complexcity error using StringBuilder tech

--- src/com/queue_program/First_non_repeating_Character_in_a_stream.java
+++ src/com/queue_program/First_non_repeating_Character_in_a_stream.java
@@ -5,47 +5,41 @@ import java.util.HashMap;
 import java.util.Queue;
 
 public class First_non_repeating_Character_in_a_stream {
-	 public static  String FirstNonRepeating(String A)
-	    {
-		 
-		 //creating the map
-		 HashMap <Character,Integer> map = new HashMap<>();
-		 Queue<Character> queue =new ArrayDeque<>();
-		 
-		 String ans = "";
-		 char StringToChar[] = A.toCharArray();
-		
-		for(int i = 0;i< StringToChar.length;i++)
-		{
-			//get the first character of the String
+	public static String FirstNonRepeating(String A) {
+
+		// creating the map
+		HashMap<Character, Integer> map = new HashMap<>();
+		Queue<Character> queue = new ArrayDeque<>();
+
+		StringBuilder ans = new StringBuilder();
+		char StringToChar[] = A.toCharArray();
+		for (int i = 0; i < StringToChar.length; i++) {
+			// get the first character of the String
 			char ch = StringToChar[i];
-			  map.put(ch, map.getOrDefault(ch, 0) + 1);
-			
-			//push into the queue
+			map.put(ch, map.getOrDefault(ch, 0) + 1); // in me when want to increase the counter i used map.pu(ch,count++)which not increment the counter 
+
+			// push into the queue
 			queue.add(ch);
-			
-			while(!queue.isEmpty())
-			{
-				
-				if(map.get(queue.peek())>1)
-				{
-					//repeating character found
+
+			while (!queue.isEmpty()) {
+
+				if (map.get(queue.peek()) > 1) {
+					// repeating character found
 					queue.remove();
-				}else
-				{
-					ans = ans + queue.peek();
+				} else {
+					ans = ans.append(queue.peek());
 					break;
 				}
 			}
-			if(queue.isEmpty())
-			{
-				ans = ans + '#';
+			if (queue.isEmpty()) {
+				ans = ans.append('#');
 			}
 		}
-			return ans;
-	    }
-	 public static void main(String[] args) {
-		String str =  "aabc";
+		return ans.toString();
+	}
+
+	public static void main(String[] args) {
+		String str = "aabc";
 		System.out.println(FirstNonRepeating(str));
 	}
 }
