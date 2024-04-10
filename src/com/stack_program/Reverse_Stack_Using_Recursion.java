4c664663b8b8d333a68f8394fe03d63ec69f1419
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-04-10 16:39:28 +0530

    Using preveous method InsertAtBottom and send one by one digit

--- src/com/stack_program/Reverse_Stack_Using_Recursion.java
+++ src/com/stack_program/Reverse_Stack_Using_Recursion.java
@@ -1,60 +1,46 @@
 package com.stack_program;
 
 import java.util.Stack;
+
 public class Reverse_Stack_Using_Recursion {
 
-	/*
 	public static void reverseStack(Stack<Integer> stack) {
-        if (!stack.isEmpty()) {
-            // Remove the top element
-            int temp = stack.pop();
-            // Reverse the remaining stack
-            reverseStack(stack);
-            // Insert the top element at the bottom
-            insertAtBottom(stack, temp);
-        }
-    }
+		if (!stack.isEmpty()) {
+			// Remove the top element
+			int temp = stack.pop(); // Reverse the remaining stack
 
-    private static void insertAtBottom(Stack<Integer> stack, int value) {
-        if (stack.isEmpty()) {
-            // If the stack is empty, insert the value
-            stack.push(value);
-        } else {
-            // Remove the top element
-            int temp = stack.pop();
-            // Recursively insert the value at the bottom
-            insertAtBottom(stack, value);
-            // Push back the removed element
-            stack.push(temp);
-        }
-    }*/
-	
-			public static void reverseStack(Stack<Integer> stack) {
-						// write your code here
-				if(stack.isEmpty()){
-					return;
-				}
-				 int temp=stack.pop();
-				 System.out.print(temp+ " ");
-				
-				 
-				reverseStack(stack);
-		
-					}
+			reverseStack(stack); // Insert the top element at the bottom
+			insertAtBottom(stack, temp);
+		}
+	}
 
-    	
-    public static void main(String[] args) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        stack.push(4);
-        stack.push(5);
+	private static void insertAtBottom(Stack<Integer> stack, int value) {
+		if (stack.isEmpty()) { // If the stack is empty, insert the value
+			stack.push(value);
+		} else {
+			// Remove the top element
 
-        System.out.println("Original Stack: " + stack);
+			int temp = stack.pop();
+			// Recursively insert the value at thebottom
 
-        
+			insertAtBottom(stack, value);
+			// Push back the removed element
+			stack.push(temp);
 
-        System.out.print("Reversed Stack: "+ " "); reverseStack(stack);
-    }
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Stack<Integer> stack = new Stack<>();
+		stack.push(1);
+		stack.push(2);
+		stack.push(3);
+		stack.push(4);
+		stack.push(5);
+
+		System.out.println("Original Stack: " + stack);
+		reverseStack(stack);
+		System.out.print("Reversed Stack: " + stack);
+	}
 }
