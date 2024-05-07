677d9f39105f09e0c0720f41108afcaba59caa30
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-05-07 16:41:11 +0530

    preveouse question contain the arraylist instead of this question contain only one value means all other value are hide or doesn't contain in list

--- src/com/basic_flow/Start.java
+++ src/com/basic_flow/Start.java
@@ -1,17 +1,38 @@
 package com.basic_flow;
 
 public class Start {
+	
 	public static void main(String[] args) {
-		System.out.println("Program is running ");
+		System.out.println("************** Do while loop ************");
 		
-		// If  Else Loop
-		int time = 22;
-		if (time < 10) {
-		  System.out.println("Good morning.");
-		} else if (time < 18) {
-		  System.out.println("Good day.");
-		} else {
-		  System.out.println("Good evening.");
+		int i = 0;
+		do {
+		  System.out.print(" "+i);
+		  i++;
+		}
+		while (i < 5);
+		
+		System.out.println();
+		
+		System.out.println("************** for loop ************");
+		for (int j = 0; j <= 10; j = j + 2) {
+			  System.out.print(j+ " ");
+			}
+		System.out.println("************** For Each loop ************");
+		String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+		for (String k : cars) {
+		  System.out.println(k);
+		}
+		
+		System.out.println("************** while loop with continue keyword ************");
+		int n = 0;
+		while (n < 10) {
+		  if (n == 4) {
+		    n++;
+		    continue;
+		  }
+		  System.out.println(n);
+		  n++;
 		}
 	}
 
