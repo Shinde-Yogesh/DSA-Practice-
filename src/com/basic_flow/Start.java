db978c36b0f99b45e55b4c8db29dfa426ca52988
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-05-07 14:21:31 +0530

    staring the DSA pattern from start

---
+++ src/com/basic_flow/Start.java
@@ -0,0 +1,18 @@
+package com.basic_flow;
+
+public class Start {
+	public static void main(String[] args) {
+		System.out.println("Program is running ");
+		
+		// If  Else Loop
+		int time = 22;
+		if (time < 10) {
+		  System.out.println("Good morning.");
+		} else if (time < 18) {
+		  System.out.println("Good day.");
+		} else {
+		  System.out.println("Good evening.");
+		}
+	}
+
+}
