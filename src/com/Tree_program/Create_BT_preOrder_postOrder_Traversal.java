ec0eea5ebdc532ca2f6e589c81b4073fa660dd64
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-05-21 16:44:54 +0530

    the pre -> post using the hashmap to store the indexwise the value and decrease the index in every recursive call

--- src/com/Tree_program/Create_BT_preOrder_postOrder_Traversal.java
+++ src/com/Tree_program/Create_BT_preOrder_postOrder_Traversal.java
@@ -1,10 +1,10 @@
 package com.Tree_program;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class Create_BT_preOrder_postOrder_Traversal {
-	 static class Node {
+	/* Solution 1
+	static class Node {
 	        int data;
 	        Node left, right;
 
@@ -74,5 +74,64 @@ public class Create_BT_preOrder_postOrder_Traversal {
 	        Node root2 = buildTree(inorder2, postorder2, n2);
 	        printPreOrder(root2); // Output: 2 9 5 4 3
 	        System.out.println();
+	    }
+	    
+	    */
+	
+	
+	static class Node {
+	    int val;
+	    Node left;
+	    Node right;
+	    Node(int x) { val = x; }
+	}
+
+	 private HashMap<Integer, Integer> inorderMap;
+	    private int postIndex;
+
+	    public Node buildTree(int[] inorder, int[] postorder, int n) {
+	        inorderMap = new HashMap<>();
+	        for (int i = 0; i < n; i++) {
+	            inorderMap.put(inorder[i], i);
+	        }
+	        postIndex = n - 1;
+	        return buildTreeRecursive(inorder, postorder, 0, n - 1);
+	    }
+
+	    private Node buildTreeRecursive(int[] inorder, int[] postorder, int inorderStart, int inorderEnd) {
+	        if (inorderStart > inorderEnd) {
+	            return null;
+	        }
+	        
+	        int rootVal = postorder[postIndex--];
+	        Node root = new Node(rootVal);
+	        
+	        int inorderIndex = inorderMap.get(rootVal);
+	        
+	        // Recursively build the right subtree first because we are decrementing postIndex
+	        root.right = buildTreeRecursive(inorder, postorder, inorderIndex + 1, inorderEnd);
+	        root.left = buildTreeRecursive(inorder, postorder, inorderStart, inorderIndex - 1);
+	        
+	        return root;
+	    }
+	    
+	    // Function to print preorder traversal
+	    public void printPreorder(Node node) {
+	        if (node == null) {
+	            return;
+	        }
+	        System.out.print(node.val + " ");
+	        printPreorder(node.left);
+	        printPreorder(node.right);
+	    }
+
+	    // Driver code
+	    public static void main(String[] args) {
+	    	Create_BT_preOrder_postOrder_Traversal sol = new Create_BT_preOrder_postOrder_Traversal();
+	        int n = 8;
+	        int[] inorder = {4, 8, 2, 5, 1, 6, 3, 7};
+	        int[] postorder = {8, 4, 5, 2, 6, 7, 3, 1};
+	        Node root = sol.buildTree(inorder, postorder, n);
+	        sol.printPreorder(root); // Expected Output: 1 2 4 8 5 3 6 7
 	    }
 }
