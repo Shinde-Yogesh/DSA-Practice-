0546d90ebe876962c7079c07b12adf81dc80e2ac
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-06-04 18:25:47 +0530

    here the node have tree cases one for one child and other two contain two child and one is for zero child

--- src/com/Tree_program/Binary_Tree_Creation.java
+++ src/com/Tree_program/Binary_Tree_Creation.java
@@ -136,6 +136,70 @@ public class Binary_Tree_Creation {
 		}
 		return temp;
 	}
+	
+	// Delete the node from Tree
+	public static Node  deleteFromBST(Node root, int val)
+	{
+		//base case
+		if(root == null)
+		{
+			return null;
+		}
+		
+		if(root.data == val)
+		{
+			//for the 0 child
+			if((root.left ==null) && (root.right == null))
+			{
+				 root = null;
+				return root;
+			}
+			// for the 1 child
+			
+			//here two cases one for the left and other for the right
+			//for the left side 
+			if((root.left != null) && (root.right == null))
+			{
+				Node temp = root.left;
+				root = null;
+				return temp;
+			}
+			
+			//for the right child if it is not null
+			if((root.left == null)&& (root.right != null))
+			{
+				Node temp = root.right;
+				root= null;
+				return temp;
+			}
+			
+			// for the 2 child
+			
+			//two child have
+			
+			if((root.left != null ) && (root.right != null))
+			{
+				int mini = minValue(root.right).data;
+				root.data = mini;
+				root.right = deleteFromBST(root.right, mini);
+				return root;
+			}
+			
+		}
+		else if(root.data > val)
+		{
+			//left call because the value is small
+			root.left = deleteFromBST(root.left, val);
+			return root;
+		}
+		else
+		{
+			//right call because the value is small
+			root.right = deleteFromBST(root.right, val);
+			return root;
+		}
+		return root;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Enter the data to create BST:");
@@ -148,5 +212,25 @@ public class Binary_Tree_Creation {
 		
 		System.out.println("The min value : "+minValue(root).data);
 		System.out.println("The max value : "+maxValue(root).data);
+		
+		
+		System.out.println("For the delete the node ");
+		deleteFromBST(root, 30);
+		
+		System.out.println("Printing the BST:");
+		levelOrderTraversal(root);
 	}
 }
+
+/* 		
+						Tree for the deleteNode
+						
+						      50
+						  /	        \
+						30		     70
+				       /   \        /   \
+				    20    	40	   60    80
+			
+
+
+*/
