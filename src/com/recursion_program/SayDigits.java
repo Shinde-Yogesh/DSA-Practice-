c2a1d9f8fe3d4568d3d65be1d28646bf3a8df1c2
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-07-04 15:37:18 +0530

    The old case doest accept negative value so change in the negative with return the value

--- src/com/recursion_program/SayDigits.java
+++ src/com/recursion_program/SayDigits.java
@@ -1,52 +1,42 @@
 package com.recursion_program;
 
 public class SayDigits {
-	public static void sayNumber(int num , String[] array)
-	{
-		//Base case
-		if(num < 0)
-		{
-			return ;
-		}
-		
-		if(num == 0)
-		{
-			return;
-		}
-		//processing part
-		
-		int digit = num % 10;
-		num = num /10;
-		
-		//Recursive call
-		sayNumber(num, array);
-		
-		System.out.print(array[digit]+ " ");
-	}
-	
-	public static void main(String[] args) {
-		String array[] = {"zero", "one","two","three","four","five","six","seven","eight","nine"};
-		int num = -752;
-		sayNumber(num, array);
-	}
+	public static void sayNumber(int num , String[] array) {
+        // Handle negative numbers
+        if(num < 0) {
+            System.out.print("negative ");
+            num = -num;
+        }
 
+        // Base case
+        if(num == 0) {
+            return;
+        }
+
+        // Processing part
+        int digit = num % 10;
+        num = num / 10;
+
+        // Recursive call
+        sayNumber(num, array);
+
+        System.out.print(array[digit]+ " ");
+    }
+
+    public static void main(String[] args) {
+        String array[] = {"zero", "one","two","three","four","five","six","seven","eight","nine"};
+        int num = -752;
+        sayNumber(num, array);
+    }
 }
 
-
-
-					/*
-					 
-					 
-					 if call the main method then output is 
-		System.out.print(array[digit]+ " ");
-		
-		//Recursive call
-		sayNumber(num, array);
-
-----------------------------------------------OUTPUT--------------------
-										zero one 
-/calling the function again and again the base case is satisfied and then print the message output is reverse ,,,
-
-
-
-					 	*/
+/*
+ * 
+ * if call the main method then output is System.out.print(array[digit]+ " ");
+ * 
+ * //Recursive call sayNumber(num, array);
+ * 
+ * ----------------------------------------------OUTPUT-------------------- zero
+ * one /calling the function again and again the base case is satisfied and then
+ * print the message output is reverse 
+ */
