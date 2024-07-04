826858e51126d4baff10bda39e247106c85ef0de
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-07-04 17:13:05 +0530

    There are some edge case are missing like negative number and zero value

--- src/com/recursion_program/Reverse_Number.java
+++ src/com/recursion_program/Reverse_Number.java
@@ -1,19 +1,23 @@
 package com.recursion_program;
 
 public class Reverse_Number {
-	public static void reverse(int number) {
-		if (number < 10) {
-			System.out.println(number);
-			return;
-		} else {
-			System.out.print(number % 10);
-			reverse(number / 10);
-		}
+	 public static void reverse(int number) {
+	        if (number < 0) {
+	            System.out.print("-");
+	            number = -number;
+	        }
+	        if (number < 10) {
+	            System.out.println(number);
+	            return;
+	        } else {
+	            System.out.print(number % 10);
+	            reverse(number / 10);
+	        }
+	    }
 
-	}
-	public static void main(String args[])
-	{
-		reverse(135);
-	}
-
+	    public static void main(String args[]) {
+	        reverse(135);  // Expected output: 531
+	        reverse(-135); // Expected output: -531
+	        reverse(0);    // Expected output: 0
+	    }
 }
