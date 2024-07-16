a79ca0b49365ad76bc61a998e7d15aadce3306bb
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-07-16 19:31:35 +0530

    removing unused space and comment for better understanding

--- src/com/string_program/DirectionWise_Path_Count.java
+++ src/com/string_program/DirectionWise_Path_Count.java
@@ -1,37 +1,28 @@
 package com.string_program;
 
 public class DirectionWise_Path_Count {
-	public static float getPath(String path)
-	{
+	public static float getPath(String path) {
 		int x = 0;
 		int y = 0;
-		
-		for(int i = 0; i< path.length();i++ )
-		{
+
+		for (int i = 0; i < path.length(); i++) {
 			char dir = path.charAt(i);
-			//south
-			if(dir == 'S')
-			{
-				y--;
-			}
-			else if(dir == 'N')
-			{
-				y++;
-			}
-			else if(dir == 'W')
-			{
-				x--;
-			}
-			else
-			{
-				x++;
+			// south
+			if (dir == 'S') {
+				y--; // Move South
+			} else if (dir == 'N') {
+				y++; // Move North
+			} else if (dir == 'W') {
+				x--; // Move West
+			} else {
+				x++; // Move East
 			}
 		}
-		int X2= x*x;
-		int Y2 = y*y;
-		return (float)Math.sqrt(X2 + Y2);
+		int X2 = x * x;
+		int Y2 = y * y;
+		return (float) Math.sqrt(X2 + Y2);
 	}
-	
+
 	public static void main(String[] args) {
 		String path = "WNEENESENNN";
 		System.out.println(getPath(path));
@@ -59,19 +50,4 @@ public class DirectionWise_Path_Count {
  					
  
  			
- 
- 
- 
- 
- 			
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
  * */
