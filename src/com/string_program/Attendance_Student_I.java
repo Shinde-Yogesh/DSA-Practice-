66e577d7368556906b46d341d11c9f1a60574a40
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-07-16 18:25:26 +0530

    the proper naming convention

--- src/com/string_program/Attendance_Student_I.java
+++ src/com/string_program/Attendance_Student_I.java
@@ -1,83 +1,41 @@
 package com.string_program;
 
 public class Attendance_Student_I {
-	
-	//LALL
-	// *****************best Approach*******************
-	public boolean Aprroach_Two_checkRecord(String s) {
-        int co = 0,a=0;
-        for(char c : s.toCharArray())
-        {
-            if(c=='P' || c == 'A')
-                co=0;
 
-            if(c == 'L')
-                co++;
-            
-            if(co == 3)
-                return false;
-            if(c == 'A')
-                a++;
-            
-            if(a>=2)
-                return false;
-        }
-
-        return true;
-    }
-	 public static  boolean Approach_1_checkRecord(String s) {
-	        int a=0;
-	        for(int i=0;i<s.length();i++){
-	            if(s.charAt(i)=='A') a++;
-	        }
-	        if(s.contains("LLL")) return false;
-	        if(a>=2) return false;
-
-	        return true;
-	         
-	    }
-	
     public static boolean checkRecord(String s) {
         int countA = 0;
         int countL = 0;
-       int   forConse = 0;
+        int forConse = 0;
 
-        for(char a : s.toCharArray())
-        {
-            if(a == 'A')
-            {
-                    countA++;
+        // Count the number of 'A's
+        for (char a : s.toCharArray()) {
+            if (a == 'A') {
+                countA++;
             }
         }
 
-        for(char a : s.toCharArray())
-        {
-            if(a == 'L')
-            {
-                    countL++;
-            }
-            else
-            {
+        // Find the maximum number of consecutive 'L's
+        for (char a : s.toCharArray()) {
+            if (a == 'L') {
+                countL++;
+            } else {
                 forConse = Math.max(forConse, countL);
                 countL = 0;
             }
         }
-       forConse = Math.max(forConse, countL);
-        if(countA < 2 && forConse < 3)
-        {
-            return true;
-        }
-        else
-        {
-           return  false;
-        }
-        
-    }
-        
-    
-	public static void main(String[] args) {
-		String s = "LALL";
-		System.out.println(checkRecord(s));
-	}
+        // Update forConse in case the string ends with 'L'
+        forConse = Math.max(forConse, countL);
 
+        // Check if the record is valid
+        if (countA < 2 && forConse < 3) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "LALL";
+        System.out.println(checkRecord(s)); // Output: true
+    }
 }
