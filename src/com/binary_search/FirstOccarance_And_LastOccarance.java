9603914103fc365768a83713925e6cb32c6d5bb6
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-07-30 18:48:31 +0530

    the java 8 feature is used

--- src/com/binary_search/FirstOccarance_And_LastOccarance.java
+++ src/com/binary_search/FirstOccarance_And_LastOccarance.java
@@ -1,70 +1,100 @@
 package com.binary_search;
 
 public class FirstOccarance_And_LastOccarance {
-	public static int firstOccarance(int array[] , int size , int key)
-	{
-		int s = 0;
-		int e = size-1;
-		int mid = s + (e - s)/2;
-		
-		int ans = -1;
-		while(s <= e)
-		{
-			if(array[mid] == key)
-			{
-				ans = mid;
-				e = mid-1;
 
-			}
-			
-			else if(key > array[mid])
-			{
-				s = mid + 1;
-			}
-			else if(key < array[mid])
-			{
-				e = mid - 1;
-			}
-			
-			 mid = s + (e - s)/2;
-		}
-		
-		return ans;
-	}
-	public static int lastOccarance(int array[] , int size , int key)
-	{
-		int s = 0;
-		int e = size-1;
-		int mid = s + (e - s)/2;
-		
-		int ans = -1;
-		while(s <= e)
-		{
-			if(array[mid] == key)
-			{
-				ans = mid;
-				s = mid+1;
+    public static int firstOccurrence(int arr[], int size, int key) {
+        int start = 0;
+        int end = size - 1;
+        int mid;
+        int ans = -1;
 
-			}
-			
-			else if(key > array[mid])
-			{
-				s = mid + 1;
-			}
-			else if(key < array[mid])
-			{
-				e = mid - 1;
-			}
-			
-			 mid = s + (e - s)/2;
-		}
-		
-		return ans;	}
-	public static void main(String[] args) {
-		int array[] = {1, 2,3,3,3,3,3,3,3,5};
-		int key = 3;
-		System.out.println("The key : "+key+" found in index "+firstOccarance(array, array.length, key));
-		System.out.println("The key : "+key+" found in index "+lastOccarance(array, array.length, key));
-	}
+        while (start <= end) {
+            mid = start + (end - start) / 2;
 
+            if (arr[mid] == key) {
+                ans = mid;
+                end = mid - 1; // Move left to find the first occurrence
+            } else if (key > arr[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static int lastOccurrence(int arr[], int size, int key) {
+        int start = 0;
+        int end = size - 1;
+        int mid;
+        int ans = -1;
+
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+
+            if (arr[mid] == key) {
+                ans = mid;
+                start = mid + 1; // Move right to find the last occurrence
+            } else if (key > arr[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 3, 3, 3, 3, 3, 3, 5};
+        int key = 3;
+
+        int firstIndex = firstOccurrence(arr, arr.length, key);
+        int lastIndex = lastOccurrence(arr, arr.length, key);
+
+        System.out.println("The first occurrence of key " + key + " is at index " + firstIndex);
+        System.out.println("The last occurrence of key " + key + " is at index " + lastIndex);
+    }
 }
+
+/*			
+    			Approach 2
+    			
+    			package com.binary_search;
+				import java.util.Arrays;
+				import java.util.OptionalInt;
+				
+				public class FirstAndLastOccurrence {
+				
+				    public static OptionalInt firstOccurrence(int[] arr, int key) {
+				        return Arrays.stream(arr)
+				                     .boxed()
+				                     .filter(value -> value == key)
+				                     .findFirst()
+				                     .map(val -> Arrays.asList(arr).indexOf(val));
+				    }
+				
+				    public static OptionalInt lastOccurrence(int[] arr, int key) {
+				        return Arrays.stream(arr)
+				                     .boxed()
+				                     .filter(value -> value == key)
+				                     .reduce((first, second) -> second)
+				                     .map(val -> Arrays.asList(arr).lastIndexOf(val));
+				    }
+				
+				    public static void main(String[] args) {
+				        int[] arr = {1, 2, 3, 3, 3, 3, 3, 3, 3, 5};
+				        int key = 3;
+				
+				        OptionalInt firstIndex = firstOccurrence(arr, key);
+				        OptionalInt lastIndex = lastOccurrence(arr, key);
+				
+				        System.out.println("The first occurrence of key " + key + " is at index " + (firstIndex.isPresent() ? firstIndex.getAsInt() : -1));
+				        System.out.println("The last occurrence of key " + key + " is at index " + (lastIndex.isPresent() ? lastIndex.getAsInt() : -1));
+				    }
+				}
+
+   // This approach leverages Java 8's functional programming features to achieve the same goal as the binary search but in a more concise and expressive manner.
+// OptionalInt is used to handle the possibility that the key might not be found in the array.
+    			*/
